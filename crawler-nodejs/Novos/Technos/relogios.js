6ff0e43b847657e0a11758e172c3d7790cc4b3f3
Matheus <mamarques012@gmail.com>
2018-11-23 01:29:51 -0200

    Parser para html

---
+++ crawler-nodejs/Novos/Technos/relogios.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const cheerio = require('cheerio');
+
+var jsonObject = [];
+var fileName = "technos-relogios.json";
+var stringHtml;
+
+
+fs.readFile('relogios-page.html', 'utf-8', function(err, data){
+  var linhas = data.split(/\r?\n/);
+  linhas.forEach(function(linha){
+     //console.log(linha);
+     stringHtml = stringHtml + linha;
+  })
+
+  const $ = cheerio.load(stringHtml);
+  var whitoutSpace;
+  $(".product").each(function() {
+    whitoutSpace = $(this).children('.product__group').children('.product__price').children('.product__price--installment').text();
+    jsonObject.push({
+      nome: $(this).children('h2').children('a').attr('title'),
+      preco: $(this).children('.product__group').children('.product__price').children('.product__price--bestprice').text(),
+      colecao: '',
+      marca: 'Technos',
+      parcelas: whitoutSpace.replace(/\s/g, '')
+    });
+
+  });
+
+  var json = JSON.stringify(jsonObject);
+  console.log(jsonObject);
+  fs.createWriteStream(fileName).write(json);
+
+})
+
+// c1.queue({
+//   uri: ""
+// });
